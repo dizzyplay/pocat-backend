1643c2120a8ea274d63e04f46ef16a27339e8f1a
dizzyplay <vovoboss@gmail.com>
2019-04-10 12:34:28 +0900

    cat weight list

---
+++ src/api/CatWeight/catWeightList/catWeightList.ts
@@ -0,0 +1,19 @@
+import { CatWeight } from "../../../entity/Cat/CatWeight";
+import { isAuthenticated } from "src/utils";
+
+export default {
+  Query: {
+    catWeightList: async (_, args, { request, connection }) => {
+      const { user } = request;
+      isAuthenticated(user);
+      const { catId, limit } = args;
+      const catWeightRepo = connection.getRepository(CatWeight);
+      return await catWeightRepo.find({
+        where: { cat: catId },
+        take: limit,
+        order: { createdAt: "DESC" },
+        relations: ["cat"]
+      });
+    }
+  }
+};
