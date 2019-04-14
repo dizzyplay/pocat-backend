ee83c47c75bdac1403b29b3dc6d4558e1bf8505c
vovo <vovoboss@gmail.com>
2019-04-14 15:51:58 +0900

    query to mutation

--- src/api/User/userLogin/userLogin.ts
+++ src/api/User/userLogin/userLogin.ts
@@ -2,14 +2,14 @@ import { User } from "src/entity/User";
 import { checkPassword, generateToken } from "src/utils";
 
 export default {
-  Query: {
+  Mutation: {
     userLogin: async (_, args) => {
       const { email, password }: { email: string; password: string } = args;
       try {
         const user = await User.findOne({ email });
         const res = await checkPassword(password, user.password);
         if (res) {
-          return generateToken(user.uuid);
+          return { token: generateToken(user.uuid) };
         } else return "password is not valid.";
       } catch {
         throw Error("user information wrong");
