6922495179216ed24a8c4d19252c0fea464206ed
dizzyplay <vovoboss@gmail.com>
2019-05-24 01:59:14 +0900

    fix default sorting

--- src/api/CatWeight/catWeightList/catWeightList.ts
+++ src/api/CatWeight/catWeightList/catWeightList.ts
@@ -1,19 +1,19 @@
-import { CatWeight } from "../../../entity/Cat/CatWeight";
-import { isAuthenticated } from "src/utils";
+import {CatWeight} from '../../../entity/Cat/CatWeight';
+import {isAuthenticated} from 'src/utils';
 
 export default {
   Query: {
-    catWeightList: async (_, args, { request, connection }) => {
-      const { user } = request;
+    catWeightList: async (_, args, {request, connection}) => {
+      const {user} = request;
       isAuthenticated(user);
-      const { catId, limit } = args;
+      const {catId, limit} = args;
       const catWeightRepo = connection.getRepository(CatWeight);
       return await catWeightRepo.find({
-        where: { cat: catId },
+        where: {cat: catId},
         take: limit ? limit : 5,
-        order: { createdAt: "DESC" },
-        relations: ["cat"]
+        order: {createdAt: 'ASC'},
+        relations: ['cat'],
       });
-    }
-  }
+    },
+  },
 };
